297dae6f5456a234fcb7611d9faf1198eea397ea
tianlinliu <tianlin@Tianlins-MacBook-Pro.local>
2014-07-20 18:34:31 -0400

    update

--- 7.Recursion/BinarySearch_revised.java
+++ 7.Recursion/BinarySearch_revised.java
@@ -1,4 +1,4 @@
-public class Test {
+public class BinarySearch_revised {
     public static int binarySearch(int[] array, int target) {
         return binarySearch(array, target, 0, array.length - 1);
     }
@@ -6,22 +6,21 @@ public class Test {
     public static int binarySearch(int[] array, int target, int lower, int upper) {
         if (lower > upper) return -1;
 
-        int mid = (upper + lower) / 2;
+        int mid = (upper - lower) / 2 + lower;
         if (array[mid] == target) {
             return mid;
         } else if (array[mid] < target) {
             return binarySearch(array, target, mid + 1, upper);
-        } else if (array[mid] > target) {
+        } else {
             return binarySearch(array, target, lower, mid - 1);
         }
-        return -1;
     }
 
     public static int binarySearch_iteration(int[] array, int target) {
         int lower = 0;
         int upper = array.length - 1;
         while (lower <= upper) {
-            int mid = (upper + lower) / 2;
+            int mid = (upper - lower) / 2 + lower;
             if (array[mid] == target) {
                 return mid;
             } else if (array[mid] < target) {
@@ -31,5 +30,9 @@ public class Test {
             }
         }
         return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(new int[] {1, 2, 3}, 2));
     }
 }
